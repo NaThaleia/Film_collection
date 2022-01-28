f8e9df3f3704701faeba785dd3f4a5fc61a0ad51
NaThaleia <miss.mekh@gmail.com>
2022-01-28 14:37:06 +0200

    change refs

--- src/js/base/refs.js
+++ src/js/base/refs.js
@@ -1,9 +1,9 @@
 export const refs = {
-  home: document.querySelector('.home'),
-  library: document.querySelector('.library'),
-  searchMovie: document.querySelector('.search-movie'),
+  // home: document.querySelector('.home'),
+  // library: document.querySelector('.library'),
+  // searchMovie: document.querySelector('.search-movie'),
   hero: document.querySelector('.hero'),
-  watched: document.querySelector('.watched'),
-  queue: document.querySelector('.queue'),
-  nIceTeam: document.querySelector('[data-modal-team-open]'),
+  // watched: document.querySelector('.watched'),
+  // queue: document.querySelector('.queue'),
+  // nIceTeam: document.querySelector('[data-modal-team-open]'),
 };
