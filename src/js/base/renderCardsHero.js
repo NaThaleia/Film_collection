56039b14a96b42b556e6ef1fb6c19b8d1c19347d
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-29 00:54:12 +0200

    fix-bag

--- src/js/base/renderCardsHero.js
+++ src/js/base/renderCardsHero.js
@@ -1,8 +1,8 @@
 import { refs } from './refs';
 import CardsHero from '../templates/heroCards.hbs';
 
-export default function renderCardsHero(obj) {
-  const cards = CardsHero(obj.results);
+export default function renderCardsHero(arr) {
+  const cards = CardsHero(arr);
 
   /* ДОДЕЛАТЬ ШАБЛОНИЗАТОР НА КОРОТКУЮ ДАТУ */
   /* СДЕЛАТЬ ФУНКЦИЮ НА ПЕРЕКОДИРОВУ ЖАНРОВ */
