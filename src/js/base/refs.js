7b8fe012883634802d605eca4bdb87f2f411112c
Aleksandr-Pod <stdfire@gmail.com>
2022-01-28 18:16:47 +0200

    added listener functions for import
    addToWatch & addToQueue - for movie cards

--- src/js/base/refs.js
+++ src/js/base/refs.js
@@ -2,8 +2,10 @@ export const refs = {
   home: document.querySelector('.home'),
   library: document.querySelector('.library'),
   // searchMovie: document.querySelector('.search-movie'),
-  hero: document.querySelector('.hero'),
   // watched: document.querySelector('.watched'),
   // queue: document.querySelector('.queue'),
+    hero: document.querySelector('.hero'),
+  addToWatch: document.querySelector('.addToWatch'),
+  addToQueue: document.querySelector('addToQueue'),
   // nIceTeam: document.querySelector('[data-modal-team-open]'),
 };
