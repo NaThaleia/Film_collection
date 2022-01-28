ff74371d8fcddeedff59205e7eb3fd5f90755c8d
Aleksandr-Pod <stdfire@gmail.com>
2022-01-28 11:35:16 +0200

    Update refs.js

--- src/js/base/refs.js
+++ src/js/base/refs.js
@@ -1,9 +1,9 @@
 export const refs = {
-    home: document.querySelector('.home'),
-    library: document.querySelector('.library'),
-    searchMovie: document.querySelector('.search-movie'),
+    // home: document.querySelector('.home'),
+    // library: document.querySelector('.library'),
+    // searchMovie: document.querySelector('.search-movie'),
     hero: document.querySelector('.hero'),
-    watched: document.querySelector('.watched'),
-    queue: document.querySelector('.queue'),
-    nIceTeam: document.querySelector('.nice-team'),
+    // watched: document.querySelector('.watched'),
+    // queue: document.querySelector('.queue'),
+    // nIceTeam: document.querySelector('.nice-team'),
 };
