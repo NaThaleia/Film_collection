5bab8584737acdf642de31e99cb32107837c67a7
Aleksandr-Pod <stdfire@gmail.com>
2022-01-28 13:57:29 +0200

    added listeners handlers

--- src/js/base/eventListeners.js
+++ src/js/base/eventListeners.js
@@ -1,12 +1,19 @@
-// import onHome from './onHome';
+import onHome from '../listeners/onHome';
+import onLibrary from '../listeners/onLibrary';
+import onSearchMovie from '../listeners/onSearchMovie';
+import onHero from '../listeners/onHero';
+import onWatched from '../listeners/onWatched';
+import onQueue from '../listeners/onQueue';
+import onTeamModal from '../listeners/onTeamModal';
 
 export default function eventListeners(refs) {
-  const { home, library, searchMovie, watched, queue, nIceTeam } = refs;
+  const { home, library, searchMovie, hero, watched, queue, nIceTeam } = refs;
   //   home.addEventListener('click', onHome);
   //   library.addEventListener('click', onLibrary);
   //   searchMovie.addEventListener('submit', onSearchMovie);
+  hero.addEventListener('click', onHero);
   //   watched.addEventListener('click', onWatched);
   //   queue.addEventListener('click', onQueue);
-  //   nIceTeam.addEventListener('click', nIceTeam);
+  //   nIceTeam.addEventListener('click', onTeamModal);
   console.log('Hello World');
 }
--- src/js/base/refs.js
+++ src/js/base/refs.js
@@ -2,6 +2,7 @@ export const refs = {
   //   home: document.querySelector('.home'),
   //   library: document.querySelector('.library'),
   //   searchMovie: document.querySelector('.search-movie'),
+  hero: document.querySelector('.hero'),
   //   watched: document.querySelector('.watched'),
   //   queue: document.querySelector('.queue'),
   //   nIceTeam: document.querySelector('.nice-team'),
