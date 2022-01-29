eed23ff2c23aa014b7a5b23b58b53e3680514026
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-29 22:09:05 +0200

    add-pages-search

--- src/js/base/renderPagination.js
+++ src/js/base/renderPagination.js
@@ -1,17 +1,18 @@
 import { refs } from './refs';
 
 export default function renderPagination(maxPage, currentPage) {
-    console.log(maxPage);
-    console.log(currentPage);
-    refs.pagination.classList.remove('is-hidden');
-    refs.paginationLast.textContent = maxPage;
-    refs.paginationLast.dataset.page = maxPage;
-    const arrPage = [];
-    
-    for (let i = 2; i < maxPage; i+=1) {
-        arrPage.push(`<div class="page" data-page="${i}">${i}</div>`)
-    }
-// const text = paginationCart();
+  console.log(maxPage);
+  console.log(currentPage);
+  refs.pagination.classList.remove('is-hidden');
+  refs.paginationLast.textContent = maxPage;
+  refs.paginationLast.dataset.page = maxPage;
+  refs.paginationAll.innerHTML = '';
+  const arrPage = [];
 
-refs.paginationAll.insertAdjacentHTML('beforeend', arrPage.join(''));
+  for (let i = 2; i < maxPage; i += 1) {
+    arrPage.push(`<div class="page" data-page="${i}">${i}</div>`);
+  }
+  // const text = paginationCart();
+
+  refs.paginationAll.insertAdjacentHTML('beforeend', arrPage.join(''));
 }
