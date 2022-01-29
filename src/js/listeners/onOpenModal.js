45f383a9eaefa828cbe4acb4d96854e5a228b235
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-01-29 18:31:01 +0200

    update onOpenModal
    noScroll

--- src/js/listeners/onOpenModal.js
+++ src/js/listeners/onOpenModal.js
@@ -5,5 +5,6 @@ export default function onOpenModal() {
     console.log('Открываем модалку');
     refs.modalSearch.classList.remove('is-hidden-modal-form');
     window.addEventListener('keydown', onModalEscKeyPress);
+    document.body.style.overflow = "hidden";
     return;
 }
