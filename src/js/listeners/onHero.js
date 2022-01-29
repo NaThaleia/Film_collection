f9a9a29583727665de4c7f68d5e39ddde2ed6d91
NaThaleia <miss.mekh@gmail.com>
2022-01-29 22:22:01 +0200

    upd onHero
    --- currentTarget

--- src/js/listeners/onHero.js
+++ src/js/listeners/onHero.js
@@ -2,17 +2,11 @@ import onOpenModal from '../listeners/onOpenModal';
 
 export default function onHero(e) {
   e.preventDefault();
-  console.dir(e.target.parentElement.className);
-  const currentTarget = e.target.closest('.filmCard')
 
   if (e.target.parentElement.className !== 'hero') {
     onOpenModal();
     return;
   }
-
-
-
-
 
   return;
 }
