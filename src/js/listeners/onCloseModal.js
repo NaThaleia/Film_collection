3c276372dbe13ff8cbfec41853cbd2c2b3fa0d10
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-01-29 13:56:30 +0200

    add fn onModalEscKeyPress
    close Modal on press Esc

--- src/js/listeners/onCloseModal.js
+++ src/js/listeners/onCloseModal.js
@@ -1,7 +1,9 @@
 import { refs } from "../base/refs";
+import onModalEscKeyPress from '../listeners/onModalEscKeyPress';
 
 export default function onCloseModal() {
     console.log('Закрыть модалку');
     refs.modalSearch.classList.add('is-hidden-modal-form');
+    window.removeEventListener('keydown', onModalEscKeyPress);
     return;
 }
