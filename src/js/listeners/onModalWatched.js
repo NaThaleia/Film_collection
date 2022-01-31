6a67dedab2b956253f01376e3392805c2f3c02ec
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-01-31 12:01:17 +0200

    add film on LS
    add film on LS on click BTNs

--- src/js/listeners/onModalWatched.js
+++ src/js/listeners/onModalWatched.js
@@ -1,5 +1,19 @@
 export default function onModalWatched(myCard) {
+    console.log(myCard);
+    console.log(myCard.id);
     console.log(`onModalWatched - myCard is:${myCard}`);
-    localStorage.setItem('LibraryWatched', JSON.stringify(myCard));
+    let watchedCards = [];
+
+    if (JSON.parse(localStorage.getItem('library-watched')) === null) {
+        watchedCards.push(myCard);
+        localStorage.setItem('library-watched', JSON.stringify(watchedCards));
+        return;
+    }
+    watchedCards = JSON.parse(localStorage.getItem('library-watched'));
+    watchedCards.push(myCard);
+
+    console.log(watchedCards);
+    localStorage.setItem('library-watched', JSON.stringify(watchedCards));
+    
     return;
 }
