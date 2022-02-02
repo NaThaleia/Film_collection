0849053419780359e9776bb153ba171a4cfb08ea
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-02-03 00:57:58 +0200

    fix_bag_spin

--- src/js/listeners/onLibrary.js
+++ src/js/listeners/onLibrary.js
@@ -2,13 +2,12 @@ import { refs } from '../base/refs';
 import onLibraryChangeClass from '../changeClassHeder/changeClassToLibrery';
 import onWatched from './onWatched';
 
-
 export default function onLibrary(e) {
   refs.hero.innerHTML = '';
+  spinner('stop');
 
   onLibraryChangeClass();
   onWatched();
- 
 
   // console.log('я в библиотеке');
 
